830f42fe9f42a9c13f7845ce73a8ede8dc791a91
xiaohanlyu <47364648+xiaohanlyu@users.noreply.github.com>
2019-02-25 19:28:25 +0100

    Add files via upload

--- src/fast.m
+++ src/fast.m
@@ -9,43 +9,63 @@ imshow(img)
 result=zeros(m,n);
 
 t=40;   
-% for proof of concept, it is ok to use loop in matlab. However, for and while are extremely slow in Matlab.
-% It is a good ideal to use vectorization whenever possible.
 for i=4:m-3
     for j=4:n-3
         Ic=img(i,j);    
-        Inn=[img(i-3,j) img(i-3,j+1) img(i-2,j+2) img(i-1,j+3) ...
-             img(i,j+3) img(i+1,j+3) img(i+2,j+2) img(i+3,j+1) ...
-             img(i+3,j) img(i+3,j-1) img(i+2,j-2) img(i+1,j-3) ...
-             img(i,j-3) img(i-1,j-3) img(i-2,j-2) img(i-3,j-1)];
+        Inn=[img(i-3,j) img(i-3,j+1) img(i-2,j+2) img(i-1,j+3) img(i,j+3) img(i+1,j+3) img(i+2,j+2) img(i+3,j+1) img(i+3,j) img(i+3,j-1) img(i+2,j-2) img(i+1,j-3) img(i,j-3) img(i-1,j-3) img(i-2,j-2) img(i-3,j-1)];
         if abs(Inn(1)-Ic)<t && abs(Inn(9)-Ic)<t
            continue; 
         end   
         I1_5_9_13=[abs(Inn(1)-Ic)>t abs(Inn(5)-Ic)>t abs(Inn(9)-Ic)>t abs(Inn(13)-Ic)>t];
         if sum(I1_5_9_13)>=3
             ind=find(abs(Inn-Ic)>t);         
-            if length(ind)>=12
+            if length(ind)>=9
                result(i,j) = sum(abs(Inn-Ic));      
             end
         end
     end
 end
-% for i=4:m-3
-%     for j=4:n-3
-%         if result(i,j)~=0
-%             if max(max(result(i-2:i+2,j-2:j+2)))==result(i,j)               
-%                 [img(i-3,j), img(i-3,j+1), img(i-2,j+2), img(i-1,j+3), img(i,j+3), img(i+1,j+3), img(i+2,j+2), img(i+3,j+1), ...
-%                  img(i+3,j), img(i+3,j-1), img(i+2,j-2), img(i+1,j-3), img(i,j-3), img(i-1,j-3), img(i-2,j-2), img(i-3,j-1)]= ...
-%                  deal(255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255);
-%             end
-%         end
-%     end
-% end
-result=nonmaxima_suppression(result, 4);
-[row,col] = find(result==1);
-imshow(img);hold on;
-plot(col(:),row(:),'rx','MarkerSize',5)
-% figure;
-% I=imshow(img);
+for i=4:m-3
+    for j=4:n-3
+        if result(i,j)~=0
+            if max(max(result(i-2:i+2,j-2:j+2)))==result(i,j)               
+                [img(i-3,j), img(i-3,j+1), img(i-2,j+2), img(i-1,j+3), img(i,j+3), img(i+1,j+3), img(i+2,j+2), img(i+3,j+1), ...
+                 img(i+3,j), img(i+3,j-1), img(i+2,j-2), img(i+1,j-3), img(i,j-3), img(i-1,j-3), img(i-2,j-2), img(i-3,j-1)]= ...
+                 deal(255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255);
+            end
+        end
+    end
+end
+
+hsize = 5;
+[x,y]=meshgrid(-hsize:hsize,-hsize:hsize);
+x = x(:);y=y(:);
+id = x == 0 & y == 0;
+x(id) = []; y(id) = [];
+ results = zeros(m,n);
+for i = 4:m-3
+    for j = 4:n-3
+        if result(i,j) == 0
+            continue;
+        end
+             nn = [i+x j+y];
+        valid = nn(:,1) > 0 & nn(:,1) <= m & nn(:,2)>0 & nn(:,2) <= n;
+        nn = nn(valid,:);
+        ind0 = sub2ind([m,n],nn(:,1),nn(:,2));
+        maxval = max(result(ind0));
+        if result(i,j) > maxval
+            results(i,j) = 1;
+        else
+            result(i,j) = 0;
+        end
+    end
+end
+[row, col] = find(results == 1);
+figure
+imshow(img),title('fast corner with bresenham circle'),
+hold on
+plot(col,row, 'r*','MarkerSize',5),
+hold off
+
 end
 
--- src/fast_origion.m
+++ src/fast_origion.m
@@ -3,42 +3,66 @@ function I = fast_origion
 %   Detailed explanation goes here
 img=imread('library2.jpg');
 imshow(img)
-close all;
-[m,n]=size(img);
+
+[m n]=size(img);
 result=zeros(m,n);
 
-t=40;   
+t=65;   
 for i=4:m-3
     for j=4:n-3
         Ic=img(i,j);    
         Inn=[img(i-3,j) img(i-3,j+1) img(i-2,j+2) img(i-1,j+3) img(i,j+3) img(i+1,j+3) img(i+2,j+2) img(i+3,j+1) img(i+3,j) img(i+3,j-1) img(i+2,j-2) img(i+1,j-3) img(i,j-3) img(i-1,j-3) img(i-2,j-2) img(i-3,j-1)];
             ind=find(abs(Inn-Ic)>t);         
             if length(ind)>=12
-               result(i,j) = length(ind);      
+               result(i,j) = 1;      
             end
     end
 end
+for i=4:m-3
+    for j=4:n-3
+        if result(i,j)~=0
+            if max(max(result(i-2:i+2,j-2:j+2)))==result(i,j)  
+                [img(i-3,j), img(i-3,j+1), img(i-2,j+2), img(i-1,j+3), img(i,j+3), img(i+1,j+3), img(i+2,j+2), img(i+3,j+1), ...
+                 img(i+3,j), img(i+3,j-1), img(i+2,j-2), img(i+1,j-3), img(i,j-3), img(i-1,j-3), img(i-2,j-2), img(i-3,j-1)]= ...
+                 deal(255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255);
+            end
+        end
+    end
+end
 
-% [row,col] = find(result==1);
-% figure
-% I=imshow(img)
-% hold on;
+hsize = 5;
+[x,y]=meshgrid(-hsize:hsize,-hsize:hsize);
+x = x(:);y=y(:);
+id = x == 0 & y == 0;
+x(id) = []; y(id) = [];
+ results = zeros(m,n);
+for i = 4:m-3
+    for j = 4:n-3
+        if result(i,j) == 0
+            continue;
+        end
+             nn = [i+x j+y];
+        valid = nn(:,1) > 0 & nn(:,1) <= m & nn(:,2)>0 & nn(:,2) <= n;
+        nn = nn(valid,:);
+        ind0 = sub2ind([m,n],nn(:,1),nn(:,2));
+        maxval = max(result(ind0));
+        if result(i,j) > maxval
+            results(i,j) = 1;
+        else
+            result(i,j) = 0;
+        end
+    end
+end
 
-% for i=4:m-3
-%     for j=4:n-3
-%         if result(i,j)~=0
-%             if max(max(result(i-2:i+2,j-2:j+2)))==result(i,j)  
-%                 [img(i-3,j), img(i-3,j+1), img(i-2,j+2), img(i-1,j+3), img(i,j+3), img(i+1,j+3), img(i+2,j+2), img(i+3,j+1), ...
-%                  img(i+3,j), img(i+3,j-1), img(i+2,j-2), img(i+1,j-3), img(i,j-3), img(i-1,j-3), img(i-2,j-2), img(i-3,j-1)]= ...
-%                  deal(255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255);
-%             end
-%         end
-%     end
-% end
-result=nonmaxima_suppression(result, 3);
-[row,col] = find(result==1);
-imshow(img);hold on;
-plot(col(:),row(:),'rx','MarkerSize',5)
+
+[row, col] = find(results == 1);
+figure
+imshow(img),title('fast corner with bresenham circle'),
+hold on
+plot(col,row, 'r*','MarkerSize',5),
+hold off
+
+
 
 end
 
