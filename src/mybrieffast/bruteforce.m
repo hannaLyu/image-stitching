d98a0e3ba1fe2d7ab173d4af8c57d88c66b8e264
xiaohanlyu <47364648+xiaohanlyu@users.noreply.github.com>
2019-03-04 21:19:44 +0100

    Add files via upload

---
+++ src/mybrieffast/bruteforce.m
@@ -0,0 +1,15 @@
+function varargout = bruteforce(descriptor1, descriptor2)
+
+        n1 = size(descriptor1,1);
+        n2 = size(descriptor2,1);
+        matching_pairs = zeros(n1,2);
+        for i = 1:n1
+            descriptor1rep = repmat(descriptor1(i,:),n2,1);
+            despxor = xor(descriptor1rep,descriptor2);
+            despxor = sum(despxor,2);
+            [~,id] = min(despxor);
+                matching_pairs(i,:) = [i, id];
+             end
+       
+    varargout{1} = matching_pairs;
+end
