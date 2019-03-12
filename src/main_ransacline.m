f7c37a21b94024bf36b9cf7baf04fdbed7524918
xiaohanlyu <47364648+xiaohanlyu@users.noreply.github.com>
2019-03-12 20:04:17 +0100

    Add files via upload

---
+++ src/main_ransacline.m
@@ -0,0 +1,32 @@
+clc;close all;clear all;
+
+% addpath(genpath('./'));
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% either load or call gen_line_data()
+% load ../data/LineData
+% X = gen_line_data(500);
+
+
+%for line
+X = gen_line_data(500);
+ figure;
+ plot(X(1,:),X(2,:),'o');hold on; % show points
+ 
+[result,linepara]=tohomogeneous(X)
+
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Results Visualization
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ plot_line_case(X,result,linepara);
+
+
+
+
+
+
+
