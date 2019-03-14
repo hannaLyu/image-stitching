ac4a6fc39c26a4ad3f8bb566142de507bf43cbf9
xiaohanlyu <47364648+xiaohanlyu@users.noreply.github.com>
2019-03-14 18:02:33 +0100

    Add files via upload

--- src/tohomogeneousplane.m
+++ src/tohomogeneousplane.m
@@ -1,4 +1,4 @@
-function [result]=tohomogeneous(X)
+function [result]=tohomogeneousplane(X)
 number = size(X,2); % total points
 sigma =0.1;   %threshold
  pretotal=0;  %points which meet requirement
@@ -14,11 +14,10 @@ for i=1:iter
    p1=[x(1);y(1);z(1);1];
    p2=[x(2);y(2);z(2);1];
    p3=[x(3);y(3);z(3);1]; %caculate plane function
-   a = ((z(1)-z(2))*(y(1)-y(3)) - (z(1)-z(3))*(y(1)-y(2)))/((x(1)-x(2))*(y(1)-y(3)) - (x(1)-x(3))*(y(1)-y(2)));
-   b = ((z(1) - z(3)) - a * (x(1) - x(3)))/(y(1)-y(3));
-   c = z(1) - a * x(1) - b * y(1);
-   plane = [a b -1 c];
-   distance=abs(plane*[X; ones(1,size(X,2))]); 
+  A=transpose([p1 p2 p3]);
+  plane=null(A)
+   plane=transpose(plane);
+   distance=abs(plane*[X; ones(1,size(X,2))])./sqrt(plane(1)^2+plane(2)^2+plane(3)^2); 
    inlierIdx = find(distance<sigma);%find inlier which colse to line
    inlierNum = length(inlierIdx);  %caculate inlier'number
      if inlierNum>pretotal           
