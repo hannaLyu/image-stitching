b1c322851244e58a01e6ac71c56c1ca4e0a2a587
DTU Space drone <xiahaa@space.dtu.dk>
2019-03-19 18:15:26 +0100

    update

--- src/common/WarpNViewMod.m
+++ src/common/WarpNViewMod.m
@@ -1,50 +1,49 @@
 function Out = WarpNViewMod(H,ImL,ImR)
+    T = maketform('projective',H');
+    %% do homography transformation, find xmin, xmax, ymin, ymax
+    [ImH,XData,YData]=imtransform(ImL,T,'FillValues',[0;0;0]);
 
+    %% do the same for a mask
+    Mask=ones(size(ImL,1),size(ImL,2));%ones(480,640);
+    MaskH=imtransform(Mask,T,'FillValues',0)>0;
 
+    %% find out output image size
+    XData=[floor(XData(1)) ceil(XData(2))];
+    YData=[floor(YData(1)) ceil(YData(2))];
+    nX=max(size(ImR,2),XData(2))-min(0,XData(1));
+    nY=max(size(ImR,1),YData(2))-min(0,YData(1));
 
-T = maketform('projective',H');
-%% do homography transformation, find xmin, xmax, ymin, ymax
-[ImH,XData,YData]=imtransform(ImL,T,'FillValues',[0;0;0]);
+    OrigoWarp = [0 0];
+    OrigoRaw = [0 0];
 
-%% do the same for a mask
-Mask=ones(size(ImL,1),size(ImL,2));%ones(480,640);
-MaskH=imtransform(Mask,T,'FillValues',0)>0;
+    if XData(1) > 0
+        OrigoRaw(1) = XData(1);
+    else
+        OrigoRaw(1) = -XData(1);
+    end
+    if YData(1) > 0
+        OrigoRaw(2) = YData(1);
+    else
+        OrigoRaw(2) = -YData(1);
+    end
 
-%% find out output image size
-XData=[floor(XData(1)) ceil(XData(2))];
-YData=[floor(YData(1)) ceil(YData(2))];
-nX=max(size(ImR,2),XData(2))-min(0,XData(1));
-nY=max(size(ImR,1),YData(2))-min(0,YData(1));
-
-OrigoWarp = [0 0];
-OrigoRaw = [0 0];
-
-if XData(1) > 0
-    OrigoRaw(1) = XData(1);
-else
-    OrigoRaw(1) = -XData(1);
-end
-if YData(1) > 0
-    OrigoRaw(2) = YData(1);
-else
-    OrigoRaw(2) = -YData(1);
-end
-
-M=ones(nY,nX)*1e-3;
-M(OrigoWarp(2)+(1:size(ImH,1)),OrigoWarp(1) + (1:size(ImH,2))) = MaskH+1e-3;
-M([OrigoRaw(2)+(1:size(ImR,1))],[OrigoRaw(1)+(1:size(ImR,2))])=...
+    M=ones(nY,nX)*1e-3;
+    M(OrigoWarp(2)+(1:size(ImH,1)),OrigoWarp(1) + (1:size(ImH,2))) = MaskH+1e-3;
+    M([OrigoRaw(2)+(1:size(ImR,1))],[OrigoRaw(1)+(1:size(ImR,2))])=...
     M([OrigoRaw(2)+(1:size(ImR,1))],[OrigoRaw(1)+(1:size(ImR,2))])+1;
-Mask=zeros(nY,nX,3);
-Mask(:,:,1)=M;
-Mask(:,:,2)=M;
-Mask(:,:,3)=M;
+    Mask=zeros(nY,nX,3);
+    Mask(:,:,1)=M;
+    Mask(:,:,2)=M;
+    Mask(:,:,3)=M;
 
-Out=zeros(nY,nX,3);
-Out(OrigoWarp(2)+(1:size(ImH,1)),OrigoWarp(2) + (1:size(ImH,2)),:)=...
-    double(ImH)./Mask(OrigoWarp(2)+(1:size(ImH,1)),OrigoWarp(2) + (1:size(ImH,2)),:);
-Out([OrigoRaw(2)+(1:size(ImR,1))],[OrigoRaw(1)+(1:size(ImR,2))],:)=...
-    Out([OrigoRaw(2)+(1:size(ImR,1))],[OrigoRaw(1)+(1:size(ImR,2))],:)+...
-    double(ImR)./Mask([OrigoRaw(2)+(1:size(ImR,1))],[OrigoRaw(1)+(1:size(ImR,2))],:);
+    Out=zeros(nY,nX,3);
+    Out(OrigoWarp(2)+(1:size(ImH,1)),OrigoWarp(2) + (1:size(ImH,2)),:)=...
+        double(ImH)./Mask(OrigoWarp(2)+(1:size(ImH,1)),OrigoWarp(2) + (1:size(ImH,2)),:);
+    Out([OrigoRaw(2)+(1:size(ImR,1))],[OrigoRaw(1)+(1:size(ImR,2))],:)=...
+        Out([OrigoRaw(2)+(1:size(ImR,1))],[OrigoRaw(1)+(1:size(ImR,2))],:)+...
+        double(ImR)./Mask([OrigoRaw(2)+(1:size(ImR,1))],[OrigoRaw(1)+(1:size(ImR,2))],:);
+    
+    Out=uint8(Out);
 
 % %% if x>0, than origo x is x, otherwise
 % % Origo=[-XData(1) -YData(1)];
