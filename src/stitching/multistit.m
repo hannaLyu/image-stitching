a31bfe68e86535e5e4298058dc7d313f3d17abdc
xiaohanlyu <47364648+xiaohanlyu@users.noreply.github.com>
2019-03-30 15:44:15 +0100

    Add files via upload

--- src/stitching/multistit.m
+++ src/stitching/multistit.m
@@ -1,8 +1,9 @@
 clc;clear;
+figure;hold on;
 imgpath = 'M:\Documents\stitching\stitching\hill\hill';  
 buildingScene = imageDatastore(imgpath);
 montage(buildingScene.Files);
-
+hold on
 numImages = numel(buildingScene.Files);
 
 
@@ -19,16 +20,28 @@ im1 = readimage(buildingScene, n);
         im1gray = im1;
     end
  img1 = single(im1gray);
-
 corner = fast(img1);
-
 descriptor = extractdes(img1,corner,patten);
-    seq.features{n} = corner;
+    seq.corner{n} = corner;
     seq.descriptors{n} = descriptor;
     seq.imgs{n} = im1;
     imageSize(n,:) = size(im1gray);
 end
+%check answer
+subplot(2,2,1);
+imshow(seq.imgs{1,1});
+hold on
 
+plot(seq.corner{1,1}(:,2),seq.corner{1,1}(:,1),'o');
+subplot(2,2,2);
+imshow(seq.imgs{2,1});
+hold on
+plot(seq.corner{1,2}(:,2),seq.corner{1,2}(:,1),'o');
+subplot(2,2,3);
+imshow(seq.imgs{3,1});
+hold on
+plot(seq.corner{1,3}(:,2),seq.corner{1,3}(:,1),'o');
+% match
 seq.matchingpair=cell(numImages,1);
 pairs = nchoosek(1:numImages,2);
 for i = 1:size(pairs,1)
