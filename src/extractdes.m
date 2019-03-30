74ebc247359896b19461b2e089693987a36e8eae
xiaohanlyu <47364648+xiaohanlyu@users.noreply.github.com>
2019-03-30 15:36:05 +0100

    Add files via upload
    fixed bug

---
+++ src/extractdes.m
@@ -0,0 +1,33 @@
+function descriptor = extractdes(im,corner,patten)
+    [M,N] = size(im);
+    g = fspecial('gaussian', [3,1], 1);
+    I = imfilter(im,g,'replicate');
+    I = imfilter(im,g','replicate');
+        descriptor = zeros(size(corner,1), size(patten,2),'logical');
+
+    for i = 1:size(corner,1)
+       
+        u1 = corner(i,1) + patten(1,:);
+        v1 = corner(i,2) + patten(2,:);
+        
+        u2 = corner(i,1) + patten(3,:);
+        v2 = corner(i,2) + patten(4,:);
+        
+        % out of border, skip
+        invalid1 = find(u1 < 1 | u1 > M | v1 < 1 | v1 > N,1);
+        invalid2 = find(u2 < 1 | u2 > M | v2 < 1 | v2 > N,1);
+
+        if isempty(invalid1) && isempty(invalid2)
+            ind1 = sub2ind([M,N],u1,v1);
+            ind2 = sub2ind([M,N],u2,v2);
+        else
+            continue;
+              error('sss');
+        end
+        desp1 = im(ind1)';
+        desp2 = im(ind2)';
+        desp = desp1 < desp2;
+        descriptor(i,:) = desp;
+    end
+end
+
