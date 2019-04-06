d5bb810a8b9cb1aaac509abdfd3422b996cf1265
xiaohanlyu <47364648+xiaohanlyu@users.noreply.github.com>
2019-04-06 19:02:35 +0200

    Add files via upload

--- src/stitching/main_stitching.m
+++ src/stitching/main_stitching.m
@@ -1,8 +1,8 @@
 
 clc;clear ;
 % corner detection
-I1=imread('1.jpg');
-I2=imread('2.jpg');
+I1=imread('hill\hill\1.JPG');
+I2=imread('hill\hill\2.JPG');
 [s11,s12,s13] = size(I1);
 [s21,s22,s23] = size(I2);
 
@@ -85,6 +85,79 @@ inlier1(1:2,:) = flipud(inlier1(1:2,:));
 inlier2(1:2,:) = flipud(inlier2(1:2,:));
 H =Hrecacl(inlier1,inlier2);
 I3 = WarpAndBlend(H,I1,I2);
-figure
-imshow(I3);
+% figure
+% imshow(I3);
 
+I4=imread('hill\hill\3.JPG');
+[s11,s12,s13] = size(I3);
+[s21,s22,s23] = size(I4);
+
+
+I3=imresize(I3,[300,400]);
+I4=imresize(I4,[300,400]);
+
+im3gray = rgb2gray(I3);
+im4gray=rgb2gray(I4);
+corner3=fast(I3);
+figure;hold on;
+corner4=fast(I4);
+patten=brief_pattern_generator;
+descriptor3=extractdes(im3gray,corner3,patten);
+descriptor4=extractdes(im4gray,corner4,patten);
+matchingpairs=bruteforce(descriptor3,descriptor4);
+imshow3 = cat(2, I3, I4);
+figure;imshow(imshow3);hold on;
+plot(corner3(:,2),corner3(:,1), 'ro','MarkerSize',3);
+plot(corner4(:,2)+size(I3,2),corner4(:,1), 'bo','MarkerSize',3);
+
+matchingpairs = matchingpairs(~isinf(matchingpairs(:,2)),:);
+
+
+shift = size(I3,2);
+cmap = jet(32);
+k = 1;
+for i = 1:size(matchingpairs,1)
+    if ~isinf(matchingpairs(i,2))
+        ptdraw = [corner3(matchingpairs(i,1),1), corner3(matchingpairs(i,1),2);
+                  corner4(matchingpairs(i,2),1), corner4(matchingpairs(i,2),2)+shift];
+        plot(ptdraw(:,2),ptdraw(:,1),'LineStyle','-','LineWidth',0.5,'Color',cmap(k,:));
+        k = mod(k+1,32);
+        if k == 0 k = 1;
+    end
+    end
+end
+
+%ransac
+matchPoints3 = transpose(corner3(matchingpairs(:,1), :));
+matchPoints4 = transpose(corner4(matchingpairs(:,2), :));
+x3 = tohomogeneous(matchPoints3);
+x4 =tohomogeneous(matchPoints4);
+
+[inlier3,inlier4,inlierId]= ransac(x3,x4);
+
+imshow4 = cat(2, I3, I4);
+figure;imshow(imshow4);hold on;
+plot(inlier3(2,:),inlier3(1,:), 'ro','MarkerSize',3);
+plot(inlier4(2,:)+size(I3,2),inlier4(1,:), 'bo','MarkerSize',3);
+
+shift = size(I3,2);
+cmap = jet(32);
+k = 1;
+for i = 1:size(inlierId,2)
+    if ~isinf(inlierId(1,i))
+        ptdraw = [x3(1,inlierId(1,i)), x3(2,inlierId(1,i));
+                  x4(1,inlierId(1,i)), x4(2,inlierId(1,i))+shift];
+        plot(ptdraw(:,2),ptdraw(:,1),'LineStyle','-','LineWidth',0.5,'Color',cmap(k,:));
+        k = mod(k+1,32);
+        if k == 0 k = 1;
+    end
+    end
+end
+
+%stitching
+inlier3(1:2,:) = flipud(inlier3(1:2,:));
+inlier4(1:2,:) = flipud(inlier4(1:2,:));
+H =Hrecacl(inlier3,inlier4);
+I5 = WarpAndBlend(H,I3,I4);
+figure;
+imshow(I5);
