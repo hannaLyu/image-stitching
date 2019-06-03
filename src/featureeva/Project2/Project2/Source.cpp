76de46417384e12dc8fcda7d2c334d2e045fc230
xiaohanlyu <47364648+xiaohanlyu@users.noreply.github.com>
2019-06-03 21:46:58 +0200

    Add files via upload

--- src/featureeva/Project2/Project2/Source.cpp
+++ src/featureeva/Project2/Project2/Source.cpp
@@ -6,49 +6,76 @@
 #include "opencv2/xfeatures2d.hpp"
 #include <iostream>  
 #include <vector> 
-//#include
-
-//#include "cv_import_static_lib.h"
 
 using namespace std;
 using namespace cv;
 
-int main(void) {
+int main() {
+	
 
-	cout << "hello opencv " << endl;
-	Mat img_1 = imread("M:\\Documents\\HelloCv\\Project2\\Project2\\KITTI\\000000.png");
-	Mat img_2 = imread("M:\\Documents\\HelloCv\\Project2\\Project2\\KITTI\\000001.png");
-
+	/*translation image*/
+	//Mat img_1 = imread("M:\\Documents\\KITTI\\000000.png");
+	//Mat img_2 = imread("M:\\Documents\\KITTI\\000001.png");
+	/*spin image*/
+	/*Mat img_1 = imread("M:\\Documents\\KITTI\\bark.120.tiff");
+	Mat img_2 = imread("M:\\Documents\\KITTI\\bark.000.tiff");*/
+	/*zoom + spin image*/
+	//Mat img_1 = imread("M:\\Documents\\KITTI\\Tiles_perspective_undistort.png");
+	//Mat img_2 = imread("M:\\Documents\\KITTI\\Tiles_perspective_distort.png");
+	/*image blur*/
+	//Mat img_1 = imread("M:\\Documents\\KITTI\\norway.jpg");
+	//Mat img_2 = imread("M:\\Documents\\KITTI\\norwayblur.jpg");
+	/*light change*/
+	Mat img_1 = imread("M:\\Documents\\KITTI\\plane1.jpg");
+	Mat img_2 = imread("M:\\Documents\\KITTI\\planelighter.jpg");
 	if (!img_1.data || !img_2.data)
 	{
 		cout << "error reading images " << endl;
+		waitKey(0);
 		return 0;
 	}
+    
 
 
 	/*feature detect*/
 	vector<KeyPoint> keyPoints_1, keyPoints_2;
 	Mat descriptors_1, descriptors_2;
+	//define feature point type
+	int n = 3;
+
+
 
 	/*-----------------FAST featrue Point----------------*/
-	//Ptr<FastFeatureDetector> detector_FAST = FastFeatureDetector::create();
-	//detector_FAST->detect(img_1, keyPoints_1);
-	//detector_FAST->detect(img_2, keyPoints_2);
-	//Ptr<ORB> descriptor_ORB = ORB::create();
-	//descriptor_ORB->compute(img_1, keyPoints_1, descriptors_1);
-	//descriptor_ORB->compute(img_2, keyPoints_2, descriptors_2);
+	if (n == 1) {
+		Ptr<FastFeatureDetector> detector_FAST = FastFeatureDetector::create();
+		detector_FAST->detect(img_1, keyPoints_1);
+		detector_FAST->detect(img_2, keyPoints_2);
+		Ptr<ORB> descriptor_ORB = ORB::create();
+		descriptor_ORB->compute(img_1, keyPoints_1, descriptors_1);
+		descriptor_ORB->compute(img_2, keyPoints_2, descriptors_2);
+		cout << "FAST feature point " << endl;
+	}
+	
+	/*-----------------SURF featrue Point----------------*/
+	else if(n==2){
+		cv::Ptr<cv::xfeatures2d::SURF> surf = cv::xfeatures2d::SURF::create();
+		surf->detect(img_1, keyPoints_1);
+		surf->detect(img_2, keyPoints_2);
+		surf->detectAndCompute(img_1, cv::Mat(), keyPoints_1, descriptors_1);
+		surf->detectAndCompute(img_2, cv::Mat(), keyPoints_2, descriptors_2);
+		cout << "SURF feature point " << endl;
+	}
 	/*-----------------SIFT featrue Point----------------*/
-	//cv::Ptr<cv::xfeatures2d::SIFT> sift = cv::xfeatures2d::SIFT::create();
-	//sift->detect(img_1, keyPoints_1);
-	//sift->detect(img_2, keyPoints_2);
-	//sift->detectAndCompute(img_1, cv::Mat(), keyPoints_1, descriptors_1);
-	//sift->detectAndCompute(img_2, cv::Mat(), keyPoints_2, descriptors_2);
-	/*-----------------SIFT featrue Point----------------*/
-	cv::Ptr<cv::xfeatures2d::SURF> surf = cv::xfeatures2d::SURF::create();
-	surf->detect(img_1, keyPoints_1);
-	surf->detect(img_2, keyPoints_2);
-	surf->detectAndCompute(img_1, cv::Mat(), keyPoints_1, descriptors_1);
-	surf->detectAndCompute(img_2, cv::Mat(), keyPoints_2, descriptors_2);
+	else if(n==3){
+		cv::Ptr<cv::xfeatures2d::SIFT> sift = cv::xfeatures2d::SIFT::create();
+		sift->detect(img_1, keyPoints_1);
+		sift->detect(img_2, keyPoints_2);
+		sift->detectAndCompute(img_1, cv::Mat(), keyPoints_1, descriptors_1);
+		sift->detectAndCompute(img_2, cv::Mat(), keyPoints_2, descriptors_2);
+		cout << "SIFT feature point " << endl;
+		
+	}
+
 
 	/*feature matching*/
 	BFMatcher matcher;
@@ -152,9 +179,17 @@ int main(void) {
 	// 以上两个变量保存的是左右两幅图像
 	Mat OutImage;
 	drawMatches(img_1, key1, img_2, key2, m_InlierMatches, OutImage);
-
+	if (n == 1) {
+		cout << "-- FAST InlinerCount :" << InlinerCount << endl;
+	}
+	else if (n == 2) {
+		cout << "-- SURF InlinerCount :" << InlinerCount << endl;
+	}
+	else if (n == 3) {
+		cout << "-- SIFT InlinerCount :" << InlinerCount << endl;
+	}
 	//stereoRectifyUncalibrated();
-	cout << "-- InlinerCount :" << InlinerCount << endl;
+
 	Mat img_matches;
 	drawMatches(img_1, keyPoints_1, img_2, keyPoints_2,
 		good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
